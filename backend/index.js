f84c031bc7e72b697171e7f6ea08b30694b0e9fa
Akshat Srivastava <124503187+akshxt20@users.noreply.github.com>
2026-05-01 20:05:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://task-manager-e3i4udjs0-akshxt20s-projects.vercel.app",
-    "https://task-manager-production-f9ba.up.railway.app",
-    /\.vercel\.app$/   // allows any Vercel preview deployment URLs
-  ],
-  credentials: true,
+  origin:'*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
